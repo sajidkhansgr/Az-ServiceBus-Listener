725708fe13233c3fadf0e3ba2923150e5f6af701
Sajid Ali <sajid.khan.sgr@gmail.com>
2022-06-19 12:51:11 +0530

    FirstPushForListener

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listner started.");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            string ServiceBusConnectionString = configuration["ServiceBusConnectionString"];
+            string QueueName = configuration["QueueName"];
+
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            // Register queue message handler and receive messages in a loop.
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            //await subscriptionClient.CloseAsync();
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+            //var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler);
+
+            //// Register the function that processes messages.
+            //subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var messageBody = Encoding.UTF8.GetString(message.Body);
+
+            var serviceBusMessage = JsonConvert.DeserializeObject<ServiceBusMessage>(messageBody);
+
+            Console.WriteLine($"Received message: UserInfo:{Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+           
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            var exception = exceptionReceivedEventArgs.Exception;
+
+            return Task.CompletedTask;
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Az-ServiceBus-Listener
+# azure-service-bus-listner
+Azure service bus which acts as a listener and receives the data.
